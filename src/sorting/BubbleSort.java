dc2dc0323bc6ff395a923f85642319d85ba11c9b
Hachiman101 <12989770+Hachiman101@users.noreply.github.com>
2020-06-14 20:21:16 +0200

    Add BubbleSort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class BubbleSort {
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 76, 1, 6, 23, 17, 31, 3, 14};
+        printArray(array);
+        sort(array);
+        printArray(array);
+    }
+}
